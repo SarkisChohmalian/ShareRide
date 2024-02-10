1ef7200a2acaf0abf211eb22054591fab5676111
SarkisChohmalian <sarkis.chohmalian@gmail.com>
2024-02-10 23:43:50 +0400

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -42,5 +41,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.google.android.gms:play-services-maps:18.0.1")
+    implementation("com.google.android.gms:play-services-location:18.0.0")
 
 }
