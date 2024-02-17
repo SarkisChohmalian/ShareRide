f05e7d37cf32dc474d012a1a875d2775920bdfdf
SarkisChohmalian <sarkis.chohmalian@gmail.com>
2024-02-18 00:32:05 +0400

    im tired of this shit... (calender)

--- app/src/main/java/com/example/shareride/ScheduledRidesFragment.java
+++ app/src/main/java/com/example/shareride/ScheduledRidesFragment.java
@@ -1,5 +1,6 @@
 package com.example.shareride;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +8,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.CalendarView;
 
 public class ScheduledRidesFragment extends Fragment {
 
+    private CalendarView calendarView;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_scheduled_rides, container, false);
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_scheduled_rides, container, false);
+
+        // Find the CalendarView in the layout
+        calendarView = view.findViewById(R.id.calendarView);
+
+        return view;
     }
 }
