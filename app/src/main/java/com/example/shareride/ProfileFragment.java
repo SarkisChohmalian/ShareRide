75db52876be5844c9e0768b9ddaab64dd59c19e1
SarkisChohmalian <sarkis.chohmalian@gmail.com>
2024-02-24 21:04:44 +0400

    ignore the last commit message

--- app/src/main/java/com/example/shareride/ProfileFragment.java
+++ app/src/main/java/com/example/shareride/ProfileFragment.java
@@ -7,16 +7,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class ProfileFragment extends Fragment {
+
+    private static final int REQUEST_IMAGE_PICKER = 1;
+    private ImageView imagePicker1, imagePicker2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        Button seeReviewButton = view.findViewById(R.id.see_review);
+        imagePicker1 = view.findViewById(R.id.imagepicker1);
+        imagePicker2 = view.findViewById(R.id.imagepicker2);
 
+        imagePicker1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImagePicker();
+            }
+        });
+
+        imagePicker2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImagePicker();
+            }
+        });
+
+        // Add review button functionality
+        Button seeReviewButton = view.findViewById(R.id.see_review);
         seeReviewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,5 +47,21 @@ public class ProfileFragment extends Fragment {
         });
 
         return view;
+    }
+
+    private void openImagePicker() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQUEST_IMAGE_PICKER);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_PICKER && resultCode == getActivity().RESULT_OK && data != null && data.getData() != null) {
+
+        }
     }
 }
